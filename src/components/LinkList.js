d994398b3d08731034f56c2941b4185028649337
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-09-20 16:26:39 +0200

    add data fetching with graphql

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,26 +1,39 @@
 import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 import Link from "./Link";
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
+    const FEED_QUERY = gql`
       {
-        id: "1",
-        description: "Prisma ...",
-        url: "https://www.prismagraphql.com",
-      },
-      {
-        id: "2",
-        description: "Apollo ...",
-        url: "https://www.apollographql.com/docs/react",
-      },
-    ];
+        feed {
+          links {
+            id
+            createdAt
+            url
+            description
+          }
+        }
+      }
+    `;
     return (
-      <div>
-        {linksToRender.map((link) => (
-          <Link key={link.id} link={link} />
-        ))}
-      </div>
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
+
+          const linksToRender = data.feed.links;
+
+          return (
+            <div>
+              {linksToRender.map((link) => (
+                <Link key={link.id} link={link} />
+              ))}
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
