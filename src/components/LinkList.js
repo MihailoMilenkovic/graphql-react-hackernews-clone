222cd8ce2b28863773670179ae844337cc22bb7c
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-09-27 11:47:04 +0200

    add authentication and voting for links

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,20 +3,38 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Link from "./Link";
 
-class LinkList extends Component {
-  render() {
-    const FEED_QUERY = gql`
-      {
-        feed {
-          links {
+export const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
             id
-            createdAt
-            url
-            description
           }
         }
       }
-    `;
+    }
+  }
+`;
+class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+
+    const votedLink = data.feed.links.find((link) => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+  render() {
     return (
       <Query query={FEED_QUERY}>
         {({ loading, error, data }) => {
@@ -27,8 +45,13 @@ class LinkList extends Component {
 
           return (
             <div>
-              {linksToRender.map((link) => (
-                <Link key={link.id} link={link} />
+              {linksToRender.map((link, index) => (
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
               ))}
             </div>
           );
@@ -37,5 +60,4 @@ class LinkList extends Component {
     );
   }
 }
-
 export default LinkList;
