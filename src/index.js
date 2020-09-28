032324b42ad25c6ff21c7eb09859b883f5ece6a0
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-09-28 18:23:05 +0200

    add searching,realtime updates and filtering

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { setContext } from "apollo-link-context";
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 import { AUTH_TOKEN } from "./constants";
 
 // 2
@@ -29,11 +32,29 @@ const authLink = setContext((_, { headers }) => {
 });
 
 // 3
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
 });
 
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 // 4
 ReactDOM.render(
   <BrowserRouter>
